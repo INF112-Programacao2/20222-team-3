ac608584312c5f5919bcae0dc92db1c613407fc8
pedrohenrique0511 <pedro.almeida3@ufv.br>
2022-12-06 14:13:42 -0300

    inserindo metodo set em obra.h

--- obra.h
+++ obra.h
@@ -1,19 +1,18 @@
-#ifndef INF112_OBRA_H
-#define INF112_OBRA_H
+#pragma once
 #include <iostream>
 
 class Obra {
-private:
-int id_obra;
-bool arquivada;
-std::string nome_obra;
-public:
-Obra(int id_obra_, bool arquivada_, std::string nome_obra_);
-~Obra();
-int get_id_obra();
-bool get_arquivada();
-std::string get_nome_obra();
-
+    private:
+        static int _id_geral;
+        int _id;
+        bool _arquivada;
+        std::string _nome;
+    public:
+        Obra(std::string nome);
+        ~Obra();
+        int get_id();
+        bool get_arquivada();
+        std::string get_nome();
+        void set_arquivada(bool arquivada);
 
 };
-#endif
