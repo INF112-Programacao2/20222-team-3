3083fd2abdbe97c61b9cc6f2f87c037cc937cc73
pedrohenrique0511 <pedro.almeida3@ufv.br>
2022-12-06 14:03:27 -0300

    acrescentando novos atributos e metodos a classe artista

--- artista.h
+++ artista.h
@@ -1,23 +1,27 @@
-#ifndef INF112_ARTISTA_H
-#define INF112_ARTISTA_H
+#pragma once
+
 #include <iostream>
-#include "obra.h"
+#include "Obra.h"
 
 class Artista {
 private:
-int id_artista;
-int numero_obras;
-std::string nome_artista;
-Obra **obras;//duvida
-int _popularidade;
+static int _id_geral;
+    int _id;
+    int _numero_obras;
+    std::string _nome;
+    Obra **_obras;
+    int _popularidade;
+    bool _carregado;
 
 public:
-Artista(int id_artista_, int numero_obras_, std:: string nome_artista_, int popularidade);
-~Artista();
-int get_popularidade();
-int get_numero_obras();
-int get_id_artista();
-std::string get_nome_artista();
-void ver_obras();
+    Artista(int numero_obras, std::string nome, int popularidade);
+    void carregar_sistema();
+    void descarregar_sistema();
+    ~Artista();
+    int get_popularidade();
+    int get_numero_obras();
+    int get_id();
+    std::string get_nome();
+    bool get_carregado();
+    void ver_obras();
 };
-#endif
