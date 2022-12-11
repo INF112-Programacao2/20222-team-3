a2d0b6c43c6b215c8cdeaf27b568bce4c6294ac0
thiggasz <thiagozduarte@gmail.com>
2022-12-11 17:24:09 -0300

    Alteracoes e tratamento de excecoes em movimento

--- Movimento.cpp
+++ Movimento.cpp
@@ -8,37 +8,36 @@ Movimento::Movimento(std::string nome, int numero_artistas){
     _id=_id_geral++; // +1 ao id geral e definindo esse numero como o _id do objeto
     _nome = nome; //_nome igual a string nome recebida como parametro
     _numero_artistas = numero_artistas; //o _numero_artistas é igual o numero inteiro de numero de artistas recebido como parametro
-    _carregado=false; // carregado começa como falso, afinal nao foram carregados os artistas e as respectivas obras ainda
 }
 
 void Movimento::carregar_sistema(){
     _artistas=new Artista*[_numero_artistas]; //ponteiro de ponteiro de artistas aponta para um ponteiro de tamanho _numero_artistas
-        for(int i=0;i<_numero_artistas;i++){ 
-            int popularidade; //inserindo a popularidade do artista
-            int numero_obras; //inseridondo o numero de obras, que será usado para alocar as obras nos artistas 
-            std::cin>>nome>>popularidade>>numero_obras; 
-            _artistas[i]=new Artista(nome,popularidade,numero_obras); // cada posicao do vetor estará sendo alocado um artista
+        for(int i=0;i<_numero_artistas;i++){
+            std::string nome;
+            std::string popularidade; //inserindo a popularidade do artista
+            std::string numero_obras; //inseridondo o numero de obras, que será usado para alocar as obras nos artistas 
+            std::cin.ignore();
+            std::getline(std::cin,nome);
+            std::cin>>popularidade;
+            // popularidade recebe inteiros entre 1 e 5
+            if(ler_digito(popularidade)==0)
+                throw std::exception("Popularidade de "+nome+" deve ser um inteiro entre 1 e 5.");
+            std::cin>>numero_obras;
+            if(ler_int(numero_obras)==0)
+                throw std::exception("Numero de obras de "+nome+" invalido.");
+            _artistas[i]=new Artista(nome,ler_digito(popularidade),ler_int(numero_obras)); // cada posicao do vetor estará sendo alocado um artista
             _artistas[i]->carregar_sistema(); // carregando as obras em cada artista do vetor
         }
-    _carregado=true; //definindo como true carregado
 }
 
-
-
 void Movimento::descarregar_sistema(){
-    if(!_carregado)//se carregado for falso, nao precisa descarregar nada, entao retorna
-        return;
     for(int i=0;i<_numero_artistas;i++)
         _artistas[i]->descarregar_sistema();//descarregando as obras de artista
     delete _artistas;//descarregando os artistas do vetor
-    _carregado=false; // ao descarregar define carregado como falso
 }
 
-
 Movimento::~Movimento(){
-    if(_carregado)
-        descarregar_sistema(); //utilizando o metodo pra remover da memoria o que foi alocado dinamicamente
-
+    descarregar_sistema(); //utilizando o metodo pra remover da memoria o que foi alocado dinamicamente
 }
 
 int Movimento::get_numero_artistas(){
@@ -47,34 +46,21 @@ int Movimento::get_numero_artistas(){
 
 void Movimento::ver_artistas(){
     for(int i = 0; i<_numero_artistas; i++){
-
         std::cout << _artistas[i]->get_nome() << std::endl; //imprimindo cada posição do vetor retornando get_nome
-
     }
 }
 
 int Movimento::calcular_popularidade(){
-    
-    int media; //onnde vai ser guardado a soma das popularidades dos artistas
-
+    int media; //onde vai ser guardado a soma das popularidades dos artistas
     for(int i = 0; i < _numero_artistas; i++){
-
         media += _artistas[i]->get_popularidade();
-
     }
-
-    return media/_numero_artistas; //retornando a popularidade os artistas somados / pelo numero de artistas, assim retornando a popularidade do movimento
-
+    return media/_numero_artistas; //retornando a popularidade os artistas somados / numero de artistas, assim retornando a popularidade do movimento
 }
 
 void Movimento::ver_obras(){
 
     for(int i = 0; i<_numero_artistas; i++){
-
         _artistas[i]->ver_obras(); //utilizando o metodo ver_obras de artistas para acesar e imprimir o nome das obras de cada artista de cada posição do vetor
     }
-}
-
-bool get_carregado(){
-    return _carregado; // retorna se foi carregado ou nao
 }
--- Movimento.h
+++ Movimento.h
@@ -11,17 +11,14 @@ class Movimento
         std::string _nome; //nome do movimento
         int _numero_artistas; // quantidade de artistas daquele movimento
         Artista** _artistas; //ponteiro de ponteiro de artistas para alocar dinamicamente os artistas que fazem parte do movimento
-        bool _carregado; // variavel que serve para conferir se esta carregado o sistema
     public:
         Movimento(std::string nome, int numero_artistas); //₢osntrutor que recebe nome, e numero de artistas 
         void carregar_sistema();  //para preencher o acervo de artistas e obras
         void descarregar_sistema(); // descarregar o acervo de artistas e obras
-        ~Movimento(); // destrutor da classe
+        virtual ~Movimento(); // destrutor da classe
         int get_numero_artistas(); //retorna o numero de artistas
-        bool get_carregado(); //retorna se foi carregado ou nao
         void ver_obras(); //imprime as obras do movimento
         void ver_artistas();//imprime os artistas do movimentp
         int calcular_popularidade();// metoodo que calcula e retorna a popularidade do movimento em relação aos artistas
 
 };
-
