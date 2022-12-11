0f607272ec16912945be35045315d1eb87cb1606
pedrohenrique0511 <pedro.almeida3@ufv.br>
2022-12-11 10:35:54 -0300

    comentários e identação em obra.h

--- Obra.h
+++ Obra.h
@@ -1,18 +1,18 @@
 #pragma once
 #include <iostream>
 
-class Obra {
-    private:
-        static int _id_geral;
-        int _id;
-        bool _arquivada;
-        std::string _nome;
-    public:
-        Obra(std::string nome);
-        ~Obra();
-        int get_id();
-        bool get_arquivada();
-        std::string get_nome();
-        void set_arquivada(bool arquivada);
-
+class Obra
+{
+private:
+    static int _id_geral;
+    int _id;           // id da obra
+    bool _arquivada;   // arquivada ou nao
+    std::string _nome; // nome da obra
+public:
+    Obra(std::string nome);             /// metodo construtor que passa nome da obra como parametro
+    ~Obra();                            // metodo destrutor
+    int get_id();                       // retorna o id da obra
+    bool get_arquivada();               // retorna se a obra esta arquivada ou nao
+    std::string get_nome();             // retorna o nome da obra
+    void set_arquivada(bool arquivada); // altera arquivada
 };
