12bbe4ec2a8f9bc02c1122d433e29ad3e0042fbc
pedrohenrique0511 <pedro.almeida3@ufv.br>
2022-12-11 16:08:53 -0300

    identação e comentários em obra.h e obra.cpp

--- Obra.cpp
+++ Obra.cpp
@@ -1,25 +1,29 @@
 #include "Obra.h"
 
-int Obra::_id_geral=100;
+int Obra::_id_geral = 100;
 
-Obra::Obra(std::string nome)
+Obra::Obra(std::string nome) // metodo construtor
 {
-    _id=_id_geral++;
-    _arquivada=false;
-    _nome=nome;
+    _id = _id_geral++;
+    _arquivada = false;
+    _nome = nome;
 }
 
-int Obra::get_id(){
+int Obra::get_id()
+{ // retorna o id da obra
     return _id;
 }
-std::string Obra::get_nome(){
+std::string Obra::get_nome()
+{ // retorna o nome da obra
     return _nome;
 }
-bool Obra::get_arquivada(){
+bool Obra::get_arquivada()
+{ // retorna um booleano se a obra esta arquivada ou nao
     return _arquivada;
 }
-void Obra::set_arquivada(bool arquivada){
-    _arquivada=arquivada;
+void Obra::set_arquivada(bool arquivada)
+{ // alteracao de arquivada
+    _arquivada = arquivada;
 }
-Obra::~Obra()
-{};
+Obra::~Obra() // metodo destrutor
+    {};
